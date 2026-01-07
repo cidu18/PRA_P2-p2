6ce57a69c982722761829c39ac9fa04ebea89740
cidu18 <a.pallas.catala@gmail.com>
2026-01-07 01:41:22 +0100

    BSTree implementado y funcionando correctamente

---
+++ BStree.h
@@ -0,0 +1,166 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+
+#include <ostream>
+#include <stdexcept>
+#include "BSNode.h"
+
+template <typename T> 
+class BSTree {
+    private:
+        int nelem;
+        BSNode<T>* root;
+        BSNode<T>* search(BSNode<T>* n, T e) const{
+          if(n == nullptr){
+            throw std::runtime_error("No se ha encontrado el elemento");
+          } else if (n->elem < e){
+              return search(n->right, e);
+          } else if (n->elem > e){
+              return search(n->left, e);
+          } else{return  n;}
+
+        }
+        BSNode<T>* insert(BSNode<T>* n, T e){
+          if(n == nullptr){
+            return new BSNode<T>(e);
+          } else if(n->elem == e){
+              throw std::runtime_error("El elemento ya existe");
+          } else if(n->elem < e){
+              n->right = insert(n->right, e);
+           } else{
+              n->left = insert(n->left, e);
+           }
+          return n;
+          
+
+
+        }
+        void print_inorder(std::ostream &out, BSNode<T>* n) const{
+          if( n != nullptr){
+            print_inorder(out, n->left);
+
+            out << n->elem << " "; 
+            
+            print_inorder(out, n->right);
+          }
+
+        }
+        BSNode<T>* remove(BSNode<T>* n, T e) {
+            if (n == nullptr) {
+                throw std::runtime_error("El elemento no existe");
+            } else if (n->elem < e) {
+                n->right = remove(n->right, e);
+            } else if (n->elem > e) {
+                n->left = remove(n->left, e);
+            } else {
+                if (n->left != nullptr && n->right != nullptr) {
+                    n->elem = max(n->left);
+                    n->left = remove_max(n->left);
+                } else {
+                    BSNode<T>* temp = n;
+                    n = (n->left != nullptr) ? n->left : n->right;
+                    delete temp;
+                }
+            }
+            return n;
+        }
+        T max(BSNode<T>* n) const {
+            if(n == nullptr){
+              throw std::runtime_error("El elemento no existe");
+            } else if(n->right != nullptr){
+              return max(n->right);
+            } else {
+              return n->elem;
+            }
+
+
+
+        }
+        BSNode<T>* remove_max(BSNode<T>* n){
+          if(n->right == nullptr){
+            return n->left;
+          } else {
+            n->right = remove_max(n-> right);
+            return n;
+          }
+
+        }
+        void delete_cascade(BSNode<T>* n){
+          if(n != nullptr){
+           delete_cascade(n->left);
+           delete_cascade(n->right);
+           delete n;
+          }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public:
+        BSTree(){
+          
+            root = nullptr;
+            nelem = 0;
+      
+        }
+        int size() const{
+          return nelem;
+        }
+        T search(T e) const{
+          return search(root, e)->elem;
+        } 
+        T operator[](T e) const{
+          return search(root, e)->elem;
+        }
+
+        void insert(T e){
+        
+          root = insert(root, e);
+          nelem++;
+        }
+
+        friend std::ostream& operator<<(std::ostream &out, const BSTree<T> &bst){
+          if(bst.root != nullptr){
+            bst.print_inorder(out, bst.root);
+
+          }
+          return out;
+        }
+        void remove(T e){
+          root = remove(root, e);
+          nelem--;
+        }
+        ~BSTree(){
+          delete_cascade(root);
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    
+};
+
+#endif
--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ bin/testTableEntry: testTableEntry.cpp TableE.h
 	g++ -o bin/testTableEntry testTableEntry.cpp
 bin/testHashTable: testHashTable.cpp TableE.h ../PRA_P1/ListLinked.h Dict.h
 	mkdir -p bin
-	g++ -o bin/tesHashTable testHashTable.cpp
+	g++ -o bin/testHashTable testHashTable.cpp
+bin/testBSTree: testBSTree.cpp BStree.h BSNode.h
+	mkdir -p bin
+	g++ -o bin/testBSTree testBSTree.cpp
 clean:
 	rm -rf *.o *.gch bin
